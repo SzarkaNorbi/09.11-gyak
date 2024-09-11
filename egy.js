0f0042dffc06509209d2edb9c33cda80e7d215aa
SzarkaNorbi <norbertszarkakovacs@gmail.com>
2024-09-11 12:56:04 +0200

    Update egy.js

--- egy.js
+++ egy.js
@@ -3,12 +3,6 @@ function f(s) {
     s.split('').reverse().join('');
 } 
 
-
-function j(a, b) {
-    document.querySelector('#output').innerHTML = a + b;
-}
-
-
 function k(text) {
     const length = text.length;
     document.querySelector('#output').innerText = 'The length of the text is: ' + length;
