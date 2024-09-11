824a1d4e5ee83d8591af10231010c2c7f4777a28
SzarkaNorbi <norbertszarkakovacs@gmail.com>
2024-09-11 12:58:47 +0200

    Delete egy.js

--- egy.js
+++
@@ -1,9 +0,0 @@
-function f(s) {
-    document.querySelector('#output').innerHTML = 
-    s.split('').reverse().join('');
-} 
-
-function k(text) {
-    const length = text.length;
-    document.querySelector('#output').innerText = 'The length of the text is: ' + length;
-}
